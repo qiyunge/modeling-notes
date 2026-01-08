9032e27ac6ea795fcc70f93fe1ad3bb84b97e08d
qiyunge <157900653@qq.com>
2026-01-07 23:05:13 -0500

    deployed to github. https://qiyunge.github.io/modeling-notes/

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://qiyunge.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/modeling-notes/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'qiyunge', // Usually your GitHub org/user name.
+  projectName: 'modeling-notes', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
